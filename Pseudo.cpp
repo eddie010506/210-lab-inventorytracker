605debfffcc8d836783ab7f6a9fee52b8642e788
Edward Choi <edwardchoi2001@gmail.com>
2025-11-05 22:26:37 -0800

    third commit

--- Pseudo.cpp
+++ Pseudo.cpp
@@ -1,1 +1,29 @@
-//Including 
+//Including necessary headers
+//Wire Frame for headers
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+#include <array>
+#include <fstream>    
+#include <cstdlib>    
+#include <ctime>
+using namespace std;
+// these seems to be needed for now
+
+// defining the complex data structure
+using InventoryList = std::array<std::list<std::string>, 3>;
+using StoreNetwork = std::map<std::string, InventoryList>; //
+
+// Define function: loadInitialData
+// Parameters: reference to StoreNetwork map
+// Open external file ("initial_stock.txt")
+// If file not open, print error and exit
+// Loop through at least 100 lines of the file
+// Read store_name, category_type, item_name
+// Based on category_type ("Juice", "Snacks", "Supply"):
+// Insert item_name into the correct list for the correct store name.
+// Close file
+
+
+
