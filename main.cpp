e9655e004ca1e6f5f3700716362fe60a3010bcfb
Edward Choi <edwardchoi2001@gmail.com>
2025-11-07 15:21:56 -0800

    update 3

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>    
 #include <cstdlib>    
 #include <ctime>
+#include <sstream>
 using namespace std; // copied from the pseudo
 
 
@@ -14,12 +15,33 @@ using StoreNetwork = std::map<std::string, InventoryList>; // copied from pseudo
 void loadInitialData(StoreNetwork& stores);
 
 int main() {
-
+    StoreNetwork simulationStores;
+    loadInitialData(simulationStores);//creating and calling the data structure
+    if (simulationStores.count("StoreA")){
+        cout << "loaded nicely" << endl;
+        const auto& juiceList = simulationStores["StoreA"][0];
+        if (juiceList.empty()) {
+            cout << "  (List is empty)" << endl;
+        } 
+        else {
+            for (const string& item : juiceList) {
+                cout << item << endl;
+            }
+        }
+    }
+    return 0;
 }
 
 void loadInitialData(StoreNetwork& stores) {
     ifstream inFile("initial_stock.txt");
-    
+    string line;
+    string storeName, category, itemName;
+    int itemsLoaded = 0;
+    //item will be in the format of store name, category, item name
+    while (getline(inFile, line)) {
+        stringstream ss(line);
+
+    }
     // read the file
     // cout if file empty error
     // cout if file not empty and read successfully
